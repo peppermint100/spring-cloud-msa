20170331c59a59c0115d054379bb2b04ba5039a2
peppermint100 <krpeppermint100@gmail.com>
2023-03-21 21:29:00 +0900

    part 1

---
+++ discoveryservice/src/test/java/com/peppermint100/discoveryservice/DiscoveryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peppermint100.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Spring Cloud MSA
+[인프런 스프링 클라우드 MSA 강의](https://www.inflearn.com/course/%EC%8A%A4%ED%94%84%EB%A7%81-%ED%81%B4%EB%9D%BC%EC%9A%B0%EB%93%9C-%EB%A7%88%EC%9D%B4%ED%81%AC%EB%A1%9C%EC%84%9C%EB%B9%84%EC%8A%A4/dashboard)를 들으며 배운 코드, 내용을 정리합니다.
